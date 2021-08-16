835fea499065c244b4784ae1dd88c690a24691b5
sef-computin <74347726+sef-computin@users.noreply.github.com>
2021-08-16 20:56:30 +0300

    Смена цвета гиперссылки
    При наведении курсора на новость, её активная область подсвечивается

---
+++ Framer.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class Framer
+{
+public static void main(String args[] )
+{
+JFrame f= new JFrame();
+f.setVisible(true);//setting visibility
+f.setSize(100,200);
+f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+}
